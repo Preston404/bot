b67ab6216f5dbffc28f787b0908c2556d50cb147
Preston Stephens <preston_stephens@yahoo.com>
2020-04-20 16:47:51 -0700

    basics are working, won't tele and run back yet

--- bot.py
+++ bot.py
@@ -1,25 +1,21 @@
 # TODO
-# Replace win32 stuff with pyautogui
 
 import numpy as nm
 import pytesseract
-#import win32api, win32con
 import pyautogui
 import time
 import random
 import math
 
 import cv2
-from PIL import ImageGrab
+import pyscreenshot as ImageGrab
 
-center_x = 640
-center_y = 360
-pytesseract.pytesseract.tesseract_cmd =  r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+center_x = 560
+center_y = 225
 stuff_to_click = "Coin Goblin spider At"
 
 def get_cursor_status_cap():
-    return ImageGrab.grab(bbox =(0, 35, 50, 65))
-    #return ImageGrab.grab(bbox =(0, 35, 200, 65))
+    return pyautogui.screenshot(region=(305, 65, 100, 20))
 
 def get_dialog_box_cap():
     return ImageGrab.grab(bbox =(0, 960, 600, 1050))
@@ -37,7 +33,7 @@ def get_digits_from_cap(cap):
     return pytesseract.image_to_string(nm.array(cap), lang ='eng', config='digits')
 
 def place_cursor(x,y):
-    win32api.SetCursorPos((x,y))
+    pyautogui.moveTo(x,y,0.1)
     
 def enemy_under_cursor():
     cap = get_cursor_status_cap()
@@ -49,28 +45,29 @@ def enemy_under_cursor():
     if (len(string) > 0):
         print(string)
     return False
+       
 
-def travel_cursor(x,y):
-    current_x, current_y = win32api.GetCursorPos()
-    dy = y - current_y
-    dx = x - current_x
-    steps = 10
-    for step in range(1,int(steps)):
-        next_x = current_x + int(float(dx*(step/steps))) + random.randint(0,3)
-        next_y = current_y + int(float(dy*(step/steps))) + random.randint(0,3)
-        ret = place_cursor(next_x, next_y)
-        
 def left_click(x,y):
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,int(x),int(y),0,0)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,int(x),int(y),0,0)
+    pyautogui.click(x,y)
+
+def move_down():
+    tmp_x = center_x
+    tmp_y = center_y+50
+    for x in range(5):
+        pyautogui.moveTo(tmp_x, tmp_y,1);
+        time.sleep(.1)
+        left_click(tmp_x,tmp_y)
+        time.sleep(1)
 
 def enemy_found():
     # Search in spiral pattern
-    travel_cursor(center_x,center_y)
+    place_cursor(center_x,center_y)
     degrees_init = random.randint(0,360);
     degrees = degrees_init
     magnitude = 50
     for i in range(300):
+        if (random.randint(0,65) == 1):
+            move_down()
         next_x = center_x + (magnitude * math.cos(degrees*(3.14159/180)))*2
         if degrees > 180:
             next_y = center_y + (magnitude * math.sin(degrees*(3.14159/180)))*.5
@@ -79,7 +76,7 @@ def enemy_found():
         degrees =  (degrees + 10) % 360
         if degrees == degrees_init:
             magnitude += 50
-        travel_cursor(next_x, next_y)
+        place_cursor(next_x, next_y)
         if (enemy_under_cursor()):
             left_click(next_x,next_y)
             return True
@@ -107,6 +104,7 @@ while(True):
     if(attacking):
         time.sleep(5)
     if (time.time() - time_start > 30*60) or start == True:
+        continue
         time.sleep(30)
         tele_lumby()
         time.sleep(30)
